187b6804df1356044b103b24a2f41e96f1898aeb
salesdevelopernew <salesdevelopernew@gmail.com>
2022-12-17 12:18:16 +0300

    Createrecord

---
+++ force-app/main/default/lwc/Week7Day25/lwc/createRecordAccount/createRecordAccount.js
@@ -0,0 +1,58 @@
+import { LightningElement, track } from 'lwc';
+
+import ACCOUNT_OBJECT from '@salesforce/schema/Account';
+import { createRecord } from 'lightning/uiRecordApi';
+
+export default class CreateRecordAccount extends LightningElement {
+
+    @track formdata = {};
+    /* Aim is to prepare data in this format
+    formdata = {
+        Name: "Test Account",
+        Industry: "Biotechnology",
+        AnnualRevenue: 100000000
+    }*/
+
+    changeHandler(event) {
+        const name = event.target.name;
+        const value = event.target.value;
+        this.formdata[name] = value;
+    }
+
+    cancelAccount() {
+
+    }
+
+    saveAccount() {
+        //populate rating & priority based on revenue
+        const revenue = Number(this.formdata.AnnualRevenue);
+        if(revenue >= 100000000) {
+            this.formdata["Rating"] = "Hot";
+            this.formdata["CustomerPriority__c"] = "High";
+        } else if(revenue < 100000000 && revenue >= 10000000) {
+            this.formdata["Rating"] = "Warm";
+            this.formdata["CustomerPriority__c"] = "Medium";
+        } else {
+            this.formdata["Rating"] = "Cold";
+            this.formdata["CustomerPriority__c"] = "Low";
+        }
+
+        //log the object so far
+        console.log("entered data: " + JSON.stringify(this.formdata));
+
+        //prepare record input for create record
+        const recordInput = {
+            apiName: ACCOUNT_OBJECT.objectApiName,
+            fields: this.formdata
+        }
+
+        //create record
+        createRecord(recordInput)
+            .then(result => {
+                console.log(result);
+            })
+            .catch(error => {
+                console.error(error);
+            })
+    }
+}
