ca0a55f175d4da42a634c3ec7497ae089dfa2638
salesdevelopernew <salesdevelopernew@gmail.com>
2022-12-17 19:35:13 +0300

    Create Record Combobax (Wire)

---
+++ force-app/main/default/lwc/Week7Day25/lwc/createRecordAccountCombobox/createRecordAccountCombobox.js
@@ -0,0 +1,76 @@
+import { LightningElement, track, wire } from 'lwc';
+
+import ACCOUNT_OBJECT from '@salesforce/schema/Account';
+import { createRecord } from 'lightning/uiRecordApi';
+import { getObjectInfo, getPicklistValues } from 'lightning/uiObjectInfoApi';
+import INDUSTRY_FIELD from '@salesforce/schema/Account.Industry';
+
+export default class CreateRecordAccount extends LightningElement {
+    industryOptions = [];
+    @track formdata = {};
+    /* Aim is to prepare data in this format
+    formdata = {
+        Name: "Test Account",
+        Industry: "Biotechnology",
+        AnnualRevenue: 100000000
+    }*/
+    @wire(getObjectInfo, {objectApiName: ACCOUNT_OBJECT})
+    accInfo;
+
+    @wire(getPicklistValues, {fieldApiName: INDUSTRY_FIELD, recordTypeId: '$accInfo.data.defaultRecordTypeId'})
+    picklistHandler({data, error}) {
+        if(data) {
+            this.industryOptions = data.values;
+        }
+        if(error) {
+            console.error(error);
+        }
+    }
+
+    changeHandler(event) {
+        const name = event.target.name;
+        const value = event.target.value;
+        this.formdata[name] = value;
+    }
+
+    cancelAccount() {
+        this.template.querySelector("form.accountform").reset();
+        this.formdata = {};
+    }
+
+    saveAccount() {
+        //populate rating & priority based on revenue
+        const revenue = Number(this.formdata.AnnualRevenue);
+        if(revenue >= 100000000) {
+            this.formdata["Rating"] = "Hot";
+            this.formdata["CustomerPriority__c"] = "High";
+        } else if(revenue < 100000000 && revenue >= 10000000) {
+            this.formdata["Rating"] = "Warm";
+            this.formdata["CustomerPriority__c"] = "Medium";
+        } else {
+            this.formdata["Rating"] = "Cold";
+            this.formdata["CustomerPriority__c"] = "Low";
+        }
+
+        //log the object so far
+        console.log("entered data: " + JSON.stringify(this.formdata));
+
+        //prepare record input for create record
+        const recordInput = {
+            apiName: ACCOUNT_OBJECT.objectApiName,
+            fields: this.formdata
+        }
+
+        //create record
+        createRecord(recordInput)
+            .then(result => {
+                console.log(result);
+                this.template.querySelector("form.accountform").reset();
+                this.formdata = {};
+                //add toast message
+            })
+            .catch(error => {
+                console.error(error);
+            })
+    }
+}
