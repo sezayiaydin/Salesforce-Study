6a65a87e85767d65bc2b045dd2bae519ef948891
salesdevelopernew <salesdevelopernew@gmail.com>
2022-12-18 11:51:45 +0300

    Edit Record Blog Edit Record ID

--- force-app/main/default/lwc/Week7Day24/lwc/getRecordBlogApproach2/getRecordBlogApproach2.js
+++ force-app/main/default/lwc/Week7Day24/lwc/getRecordBlogApproach2/getRecordBlogApproach2.js
@@ -10,7 +10,7 @@ import TOPIC_FIELD from '@salesforce/schema/Blog__c.Topic__c';
 const FIELDS = [AUTHOR_FIELD, NAME_FIELD, BODY_FIELD, TITLE_FIELD, TOPIC_FIELD];
 
 export default class GetRecordBlog extends LightningElement {
-    recordId = "a084x00000BkI1cAAF";
+    recordId = "a084x00000C068WAAR";
     author;
     name;
     body;
