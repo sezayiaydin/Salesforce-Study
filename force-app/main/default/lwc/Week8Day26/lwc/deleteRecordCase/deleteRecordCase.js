3090af27e681ef7e39d444df4f7ed9d424ad2ccf
salesdevelopernew <salesdevelopernew@gmail.com>
2022-12-20 10:09:47 +0300

    Delete Record

---
+++ force-app/main/default/lwc/Week8Day26/lwc/deleteRecordCase/deleteRecordCase.js
@@ -0,0 +1,27 @@
+import { deleteRecord } from 'lightning/uiRecordApi';
+import { LightningElement } from 'lwc';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+
+export default class DeleteRecordCase extends LightningElement {
+    recordId;
+
+    changeHandler(event) {
+        this.recordId = event.target.value;
+    }
+
+    deletRec() {
+        const recordId = this.recordId;
+        deleteRecord(recordId)
+            .then(result => {
+                this.displayToast("Success", "Record has been deleted successfully!", "success");
+            })
+            .catch(error => {
+                this.displayToast("Error", error.body.message, "error");
+            })
+    }
+
+    displayToast(title, message, variant) {
+        const toast = new ShowToastEvent({title, message, variant});
+        this.dispatchEvent(toast);
+    }
+}
